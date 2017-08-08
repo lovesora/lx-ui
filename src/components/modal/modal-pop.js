c12bad6fe191b2146056c03fa34512495da43817
liuxin <475212506@qq.com>
2017-08-08 16:09:36 +0800

    fixed some bugs

--- src/components/modal/modal-pop.js
+++ src/components/modal/modal-pop.js
@@ -1,5 +1,5 @@
 import Modal from './modal-defult';
-import PopConfirm from '../pop-box/pop-confirm';
+import PopBox from '../pop-box/pop-box';
 
 class ModalPop extends Modal {
     constructor (...args) {
